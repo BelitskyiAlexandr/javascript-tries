d2a462cada89af94975cca36ffd1f25a245e44d0
BelitskyiAlexandr <vilisapett@gmail.com>
2024-01-31 16:57:44 +0200

    destructuring, taking out props

---
+++ oop beginning/destructuring.js
@@ -0,0 +1,9 @@
+const user = {
+    name: "Tom",
+    age: 24,
+    phone: "+367438787",
+    email: "tom@gmail.com"
+};
+const {name, email} = user;
+console.log(name);      // Tom
+console.log(email);     // tom@gmail.com
