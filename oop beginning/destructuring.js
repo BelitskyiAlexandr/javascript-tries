104f85f4c8c87f660baca75cec5cc5b60f70fc81
BelitskyiAlexandr <vilisapett@gmail.com>
2024-01-31 16:57:48 +0200

    destructuring, taking out props

--- oop beginning/destructuring.js
+++ oop beginning/destructuring.js
@@ -1,3 +1,4 @@
+// Destructuring allows you to extract individual values from an object into variables or constants
 const user = {
     name: "Tom",
     age: 24,
@@ -7,3 +8,38 @@ const user = {
 const {name, email} = user;
 console.log(name);      // Tom
 console.log(email);     // tom@gmail.com
+//when destructuring an object, variables are placed in curly braces and assigned to the object
+//the mapping between object properties and variables/constants is by name
+
+//mapping with another name
+const {name: userName, email: mailAddress} = user;
+console.log(userName);      // Tom
+console.log(mailAddress);   // tom@gmail.com
+
+// values if cannot find properties
+const user1 = {
+    name: "Tom",
+    age: 24,
+};
+const {name1  = "Sam", email: mailAddress1 = "sam@gmail.com"} = user1;
+console.log(name1);              // Tom
+console.log(mailAddress1);       // sam@gmail.com
+
+//If a variable/constant, when destructured, is mapped to a property that represents a complex object, 
+//then after destructuring that variable/constant will also represent the complex object
+const user2 = {
+    name: "Tom",
+    age: 24,
+    account: {
+        login: "tom555",
+        password: "qwerty"
+    }
+};
+const {account} = user2;
+console.log(account.login); // tom555
+console.log(account.password); // qwerty
+
+//if need special field from complex obj
+const {name2, account: {login}} =  user2;
+console.log(name);      // Tom
+console.log(login);     // tom555
