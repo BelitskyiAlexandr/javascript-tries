7ad68838d5ed57f4ee162e02f4ea164ccf555f91
BelitskyiAlexandr <vilisapett@gmail.com>
2024-01-31 17:04:23 +0200

    destructuring, rest operator

--- oop beginning/destructuring.js
+++ oop beginning/destructuring.js
@@ -43,3 +43,15 @@ console.log(account.password); // qwerty
 const {name2, account: {login}} =  user2;
 console.log(name);      // Tom
 console.log(login);     // tom555
+
+//rest
+const tom = {
+    name: "Tom",
+    age: 24,
+    phone: "+367438787",
+    email: "tom@gmail.com"
+};
+const {name3, age, ...contacts} = tom;
+console.log(name3);      // Tom
+console.log(age);       // 24
+console.log(contacts);  // {phone: "+367438787", email: "tom@gmail.com"}    contacts - obj
